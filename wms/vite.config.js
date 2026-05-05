f9c77d7d92a1cff674dbc52f21246e4f19e09d0e
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-05 19:26:48 +0500

    Change server host in vite.config.js from '0.0.0.0' to 'localhost'

--- wms/vite.config.js
+++ wms/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     },
     server: {
         allowedHosts: true,
-        host: '0.0.0.0',
+        host: 'localhost',
         watch: { ignored: ['**/storage/framework/views/**'] },
     },
 });
