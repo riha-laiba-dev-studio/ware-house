7f95f287e76930019d58469bab53db377bf2c3d6
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-06 09:41:34 +0500

    Add CategoryController to manage category operations

---
+++ wms/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    public function store(Request $request)
+    {
+        Category::create([
+            'name' => $request->name
+        ]);
+
+        return redirect()->route('categories.index');
+    }
+}
